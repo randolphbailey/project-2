a1884a5948880b9684575bb3b09f82e98a4eef67
Dina Khouri <dina.khouri.91@gmail.com>
2019-04-03 20:50:43 -0500

    added login and register pages, and html routes for them

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -10,10 +10,18 @@ module.exports = function(app) {
       });
     });
   });
-
+  // complete those routes
+  // app.get("/login", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/login.handlebars"));
+  // });
+  // app.get("/register", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../views/register.handlebars"));
+  // });
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.render("example", {
         example: dbExample
       });
