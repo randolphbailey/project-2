f5609bf9c54e73d3425a625ac83b3c390c4ae7d8
Randolph Bailey <randolphbailey@gmail.com>
2019-04-12 10:01:51 -0500

    more variable changes, create post now also posts forum ID

--- public/js/posts.js
+++ public/js/posts.js
@@ -8,29 +8,29 @@ $(document).ready(function() {
   $("#createpost").on("click", function() {
     var titleInput = $("#post-title");
     var bodyInput = $("#postBody");
-    var forumid = $("#createpost").val();
+    var forumid = $("#forum-createPost").val();
     // var postedby = $("#createpost").attr("byid");
 
     var postData = {
       title: titleInput.val().trim(),
       body: bodyInput.val().trim(),
-      forumID: forumid
+      forumId: forumid
     };
 
     console.log(postData);
     $("#createpost").val("");
-    createPost(postData.title, postData.body, postData.forumID);
+    createPost(postData.title, postData.body, postData.forumId);
     //empty the fields
     titleInput.val("");
     bodyInput.val("");
   });
 
   // Does a post to the create route. If successful, we are redirected to the main page
-  function createPost(title, body, forumID) {
+  function createPost(title, body, forumId) {
     $.post("/api/create", {
       title: title,
       body: body,
-      id: forumID
+      id: forumId
     }).then(function() {
       window.location.reload();
     });
